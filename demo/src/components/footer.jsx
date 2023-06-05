5262f7ea6503fab7fce0cd882d94f3e6adacb913
Lotreez <lotreez@yandex.ru>
2023-06-05 15:44:07 +0300

    third commit

---
+++ demo/src/components/footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return <footer className="page-footer
+    blue lighten-2 accent-1">
+
+        <div className="footer-copyright ">
+            <div className="container">
+            Ⓒ {new Date().getFullYear()}
+            <br></br>Ustyukhin Andrey
+            <a className="grey-text
+            text-lighten-4 right"
+            href="!#">Reprository</a>
+            </div>
+        </div>
+    </footer>
+}
+
+export { Footer };
